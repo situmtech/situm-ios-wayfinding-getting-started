bc2e7653a55c8fa14868164cda7d95b71747c53a
AdrianRV <adrian.rodriguez@situm.es>
2019-06-18 18:04:52 +0200

    Add example project

---
+++ Podfile
@@ -0,0 +1,6 @@
+target 'situm-ios-wayfinding-getting-started' do
+  use_frameworks!
+  source 'https://github.com/CocoaPods/Specs.git'
+  platform :ios, '9.0'
+  pod 'SitumWayfinding', '0.1.3'
+end
---
+++ situm-ios-wayfinding-getting-started/WayfindingController.swift
@@ -0,0 +1,43 @@
+//
+//  WayfindingController.swift
+//  ios-app-wayfindingExample
+//
+//  Created by Adrián Rodríguez on 17/06/2019.
+//  Copyright © 2019 Situm Technologies. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SitumWayfinding
+import SitumSDK
+import GoogleMaps
+
+class WayfindingController: UIViewController {
+    
+    @IBOutlet var containerView: UIView!
+    
+    var library: SitumMapsLibrary?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let credentials: Credentials = Credentials(user: "YOUR_USER", apiKey: "YOUR_SITUM_APIKEY", googleMapsApiKey: "YOUR_GOOGLEMAPS_APIKEY")
+        self.library = SitumMapsLibrary(containedBy: self.containerView, controlledBy: self)
+        self.library?.setOnBackPressedCallback(
+            {(_ sender: Any) in
+                self.performSegue(withIdentifier: "unloadWayfinding", sender: self)
+            }
+        )
+        library?.load(buildingWithId: "YOUR_BUILDING_ID", logWith: credentials)
+        super.viewWillAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
+
